73b5e6179276e86295b3745c982780ba89d6380e
Enaideira <itsearthchan@gmail.com>
2022-02-21 04:58:05 -0500

    troubleshooting "Help" slash command

--- Root/Commands/SlashCommands/Basic/Help.js
+++ Root/Commands/SlashCommands/Basic/Help.js
@@ -5,12 +5,14 @@ module.exports = {
 	description: "Run this for help",
 	run: async(client, interaction, container) => {
 		const llamarama = new container.Discord.MessageEmbed()
-		.setColor('RANDOM')
+		.setColor(16220659)
+		/*.setAuthor(iconURL)*/
 		.setTimestamp()
 		.setTitle('❓ | Help!')
-		.setDescription(`Help coming soon!`)
-		.addField("**Join our support server!**", "https://discord.gg/vbs5RZD", false)
-    .setFooter(`Running with love on  ${os.type()}`);
+		.setDescription(`My Message Command prefix is the **-** key! I have several handy Slash Commands, too!`)
+		.addField("**• Join our support server!**", "You can join by clicking [here!](https://discord.gg/vbs5RZD)", false)
+		.addField("test", "test", false)
+    	.setFooter(`Running with love on  ${os.type()}`);
 		interaction.reply({ embeds: [llamarama] })
 	}
 }
