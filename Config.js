46f39bb0fdd6d6bb9e4b1ffd1d94c9ebedc733a4
Enaideira <mitch817@gmail.com>
2022-02-21 05:04:47 -0500

    added "version" & "versionName" to Config

--- Config.js
+++ Config.js
@@ -1,6 +1,8 @@
 module.exports = {
     prefix: ["PREFIX"],
     developers: ["YOUR_USER_ID"],
+    version: ["VERSION_NUMBER"],
+    versionName: ["VERSION_NAME"],
     token: "YOUR_BOT_TOKEN"
 }
 
